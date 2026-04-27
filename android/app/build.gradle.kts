6def583fa6d7b7a101c7ef9f7c0fbf4a4239714d
zhilsinger <zhilsinger@gmail.com>
2026-04-26 17:06:53 -0800

    Fix fallback secret file and Android NDK version

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
     namespace = "com.zhilsinger.kelizo"
     compileSdk = flutter.compileSdkVersion
 //    ndkVersion = flutter.ndkVersion
-    ndkVersion = "27.0.12077973"
+    ndkVersion = "28.2.13676358"
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
